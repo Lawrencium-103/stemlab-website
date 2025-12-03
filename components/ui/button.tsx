f8f86e6b5a30a4b7386b40c1897e3dfece85e910
RogmaChurch <rogmachurch@gmail.com>
2025-12-03 15:48:52 +0100

    Fix: Remove unused imports in button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+
 import { cn } from "@/lib/utils";
 
 // Note: We haven't installed cva or radix-ui/react-slot yet, but I'll implement a simpler version first 
